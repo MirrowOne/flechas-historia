f48b5c6f974b62187897c5c64e46e7dd3260753a
Jonas <jjalvarezmendoza20@gmail.com>
2025-11-27 14:49:12 -0400

    styles: mejorar estilos de arrow.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,13 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const { action: direction } = useActionDetector();
 
+  const CATCHED_ARROWS = [
+    <Arrow key={1} direction="up" />,
+    <Arrow key={2} direction="down" />,
+    <Arrow key={3} direction="left" />,
+    <Arrow key={4} direction="right" />,
+  ];
+
   useEffect(() => {
     console.log("ultima", direction);
   }, [direction]);
@@ -26,6 +33,7 @@ export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="flex flex-col items-center gap-4">
+        <section className="flex gap-5">{CATCHED_ARROWS}</section>
         <p>{getMessage()}</p>
         <Arrow direction={direction} />
       </div>
